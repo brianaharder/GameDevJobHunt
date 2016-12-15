df417e2152c447e8431ee63fced1680cbb4400f5
brianaharder <briana.harder@gmail.com>
2016-12-15 13:51:35 -0800

    Initial commit
    I don't even know how half of this works, it's been a few years since I
    wrote it, but it all seems functional...

---
+++ find_job_pages.py
@@ -0,0 +1,153 @@
+import requests
+import yaml
+from bs4 import BeautifulSoup
+
+import os.path
+
+#dumb crawler options
+url_endings = ['/','.php','.html','.asp','.htm']
+url_keywords = ['jobs', 'careers', 'career', 'Jobs', 'wp/jobs', 'company/jobs', 'company/careers', 'openings']
+keywords = ['job', 'career', 'hiring']
+
+def get_company_URLs(list_url):
+    """
+    Get company urls from gamedevmap
+    """
+    r = requests.get(list_url)
+    soup = BeautifulSoup(r.text)
+    urls = []
+    for cell in soup.find_all('b'):
+        url = cell.find('a')
+        if url:
+            urls.append(url.get('href'))
+    return urls
+
+
+def get_jobs_page(company_url):
+    """
+    Try to navigate to a jobs page from a homepage
+    """
+    try:
+        mainr = requests.get(company_url, timeout=5)
+        if company_url[-1] != r"/":
+            company_url += r"/"
+
+        # Attempt to find a link on the main page to the jobs page
+        potential_pages = []
+        for link in BeautifulSoup(mainr.text).find_all('a'):
+            is_job = False
+            if link.text is not None:
+                is_job = any([keyword == link.text.lower() for keyword in keywords])
+            if not is_job and link.find('img') is not None and link.find('img').get('alt') is not None:
+                is_job = any([keyword == link.find('img').get('alt').lower() for keyword in keywords])
+            if is_job:
+                try:
+                    if link.get('href') not in potential_pages and '@' not in link.get('href'):
+                        potential_pages.append(link.get('href'))
+                except TypeError as e:
+                    print("error on", company_url, link.text)
+                    pass
+        if len(potential_pages) >= 1:
+            print("Found link!")
+            jobpage = potential_pages[0]
+            if jobpage[0] == '.' or jobpage[0] == r'/':
+                jobpage = jobpage.strip(r'./')
+                return company_url + jobpage
+            elif jobpage[0] == "#":
+                return company_url
+            return jobpage
+
+
+
+
+        # Look for a real page in one of the common places jobs pages live
+        for ending in url_endings:
+            for keyword in url_keywords:
+                newurl = ''.join([company_url,keyword,ending])
+                r = requests.get(newurl, timeout=1)
+                if r.status_code == 200:
+                    if '404' in r.text or 'Page not found' in r.text or 'Fatal error' in r.text or '506' in r.text or "Not Found" in r.text:
+                        #print('{url} has faulty error codes'.format(url=newurl))
+                        continue
+                    elif r.text == mainr.text:
+                        #print('{url} redirects to main page on bad pages'.format(url=newurl))
+                        continue
+                    elif len(r.text) < 100 or 'temporarily unavailable' in r.text:
+                        #print('{url} not really a page'.format(url=newurl))
+                        continue
+                    else:
+                        return newurl
+        else:
+            return ""
+    except (requests.exceptions.ConnectionError, requests.exceptions.ChunkedEncodingError, requests.exceptions.TooManyRedirects, requests.exceptions.ReadTimeout) as e:
+        #print("Cannot Connect to: ", company_url, e)
+        return "Dead?"
+
+
+
+def update_site_listings(urls_file='company_urls.txt', list_url=False,force=False):
+    """
+    Updates url files from a list_url
+    Optional force to force it to regenerate ALL job page urls
+    """
+    if not os.path.exists(urls_file):
+        f = open(urls_file, 'w')
+        yaml.dump({"test": "testing"}, f)
+        f.close()
+    with open(urls_file, 'r') as company_urls:
+        data = yaml.load(company_urls)
+        #remake company urls if a list url is provided
+        if list_url:
+            new_urls = get_company_URLs(list_url)
+            for url in new_urls:
+                if url not in data and url+r"/" not in data and url.replace('https','http') not in data:
+                    data[url] = ""
+                    print('adding url', url)
+            dump_yaml(data, urls_file)
+        for url,jobspage in data.items():
+            if force or jobspage == "":
+                old = jobspage
+                new = get_jobs_page(url)
+                if new == "":
+                    new = old
+                else:
+                    data[url] = new
+                    print(url, ': ', data[url])
+            else:
+                pass
+
+    dump_yaml(data,urls_file)
+
+
+def dump_yaml(db, filename):
+    with open(filename, 'w+') as f:
+        yaml.safe_dump(db, f, encoding='utf-8', allow_unicode=True, default_flow_style=False)
+
+
+def clean_list():
+    """
+    Tidy company urls if anything strange has happened to it
+    """
+    with open('company_urls.txt', 'r') as company_urls:
+        data = yaml.load(company_urls)
+        for url, joburl in data.items():
+            print(type(joburl))
+            if joburl is None or "http" not in joburl:
+                data[url] = ""
+            if url[-1] == r'/':
+                alturl = url + r"/"
+            else:
+                alturl = url[:-1]
+            if alturl in data:
+                if len(data[alturl]) > len(joburl):
+                    data.remove(url)
+                else:
+                    data.remove(alturl)
+    dump_yaml(data, 'clean.txt')
+
+
+#clean_list()
+#update_site_listings(r"http://www.gamedevmap.com/index.php?location=&country=United%20States&state=&city=&query=&type=&start=1&count=2000", force=False)
+update_site_listings('company_urls_fails.txt')
+
+#print(get_jobs_page(r"http://pocketgems.com/"))
---
+++ scrape_job_pages.py
@@ -0,0 +1,218 @@
+import requests
+from bs4 import BeautifulSoup
+import yaml
+
+import re, os
+from collections import Counter
+
+re.IGNORECASE
+
+def add_job_to_db(job_url,job_title,job_details):
+    if job_details is None or job_title is None:
+        return False
+    if job_url not in Jobs_Database:
+        Jobs_Database[job_url] = {}
+    job_title = job_title.strip().split('\n')[0]
+    job_details = job_details.strip()
+    Jobs_Database[job_url][job_title] = job_details
+    return True
+
+
+def find_jobs_on_page(company_url,job_url):
+    jobsfound = 0
+    try:
+        r = requests.get(job_url, timeout=1)
+    except(requests.ConnectionError, requests.exceptions.ChunkedEncodingError, requests.exceptions.ReadTimeout) as e:
+        print('could not fetch pages at {url}'.format(url=company_url))
+        return False
+
+    soup = BeautifulSoup(r.text)
+    style = find_job_style_2(soup)
+    jobs = find_all_with_style(soup, style)
+
+    for job in jobs:
+        link = job.find('a')
+        if link and link.get('href'):
+            url = link.get('href').strip()
+            job_title = link.string
+        else:
+            url = job_url.strip()
+            job_title = job.string
+        if add_job_to_db(job_url,job_title,url):
+            jobsfound += 1
+    if jobsfound == 0:
+        pass
+        # print('Nothing found at {url}'.format(url=job_url))
+
+
+def find_job_style_2(soup):
+    styles = find_potential_job_styles(soup)
+    if not styles:
+        return False
+    passed_test = []
+    for style in styles:
+        matches = find_all_with_style(soup, style)
+        if any(match.string and len(match.string) > 100 for match in matches):
+            pass
+        else:
+            passed_test.append(style)
+    #if len(passed_test) > 1:
+    #    print("multiple passes", matches)
+    if len(passed_test) == 0:
+        return False
+    return Counter(passed_test).most_common(1)[0][0]
+
+
+
+def find_potential_job_styles(soup):
+    def codify_style(tag):
+        keys = []
+        for key,value in tag.attrs.items():
+            if type(key) == list:
+                keys.extend(key)
+            else:
+                keys.append(key)
+        return tag.name, tuple(keys)
+
+    def getstyles(potentialmatches):
+        if len(potentialmatches) == 0:
+            return False
+        styles = []
+        for match in potentialmatches:
+            disqualified_styles = []
+            style = []
+            for parent in match.parents:
+                style.append(codify_style(parent))
+            styles.append(tuple(style))
+        return styles
+
+    regex = '|'.join(job_words) + r'\b'
+    disqualification_regex = '|'.join(job_exceptions) + r'\b'
+    potentialmatches = [match for match in soup.find_all(text=re.compile(regex))
+                        if len(match.string) < 100 and not re.match(disqualification_regex, match.string)]
+    return getstyles(potentialmatches)
+
+
+def find_all_with_style(soup, styles):
+    def match_style(matches,style):
+        working = []
+        for original,match in matches:
+            goodmatch = True
+            if match.name == style[0]:
+                for attr in style[1]:
+                    if match.get(attr) == None:
+                        goodmatch = False
+                for attr in match.attrs:
+                    if attr not in style[1]:
+                        goodmatch = False
+                if goodmatch:
+                    working.append((original,match.parent))
+        return working
+
+    def matchattrs(matches, style):
+        working = []
+        for match in matches:
+            for attr in style[1]:
+                if match.get(attr) == None:
+                    break
+            else:
+                working.append((match, match.parent))
+        return working
+    if styles:
+        matches = soup.find_all(styles[0][0])  # MAGIC NUMBERS SORRY
+        matches = matchattrs(matches, styles[0])
+
+        for style in styles[1:-1]:  # ...why?
+            matches = match_style(matches, style)
+        return [match[0] for match in matches]
+    else:
+        return []
+
+
+def compare_joblists(old_filename,new_filename):
+    def safe_writelines(output, lines):
+        try:
+            output.writelines(lines)
+        except UnicodeEncodeError:
+            for line in lines:
+                try:
+                    output.write(line)
+                except UnicodeEncodeError:
+                    print('Could not write', line.strip())
+    class DictDiffer(object):
+        """
+        Calculate the difference between two dictionaries as:
+        (1) items added
+        (2) items removed
+        (3) keys same in both but changed values
+        (4) keys same in both and unchanged values
+        """
+        def __init__(self, current_dict, past_dict):
+            self.current_dict, self.past_dict = current_dict, past_dict
+            self.set_current, self.set_past = set(current_dict.keys()), set(past_dict.keys())
+            self.intersect = self.set_current.intersection(self.set_past)
+        def added(self):
+            return self.set_current - self.intersect
+        def removed(self):
+            return self.set_past - self.intersect
+        def changed(self):
+            return set(o for o in self.intersect if self.past_dict[o] != self.current_dict[o])
+        def unchanged(self):
+            return set(o for o in self.intersect if self.past_dict[o] == self.current_dict[o])
+
+    with open('update.txt', 'w+') as output:
+        old_data = yaml.load(open(old_filename, 'r+'))
+        new_data = yaml.load(open(new_filename, 'r+'))
+        diff = DictDiffer(new_data, old_data)
+        for company in diff.changed():
+            if not isinstance(new_data[company], dict) and not isinstance(old_data[company], dict):
+                continue
+            company_diff = DictDiffer(new_data[company],old_data[company])
+            output.write(company + '\n')
+            if company_diff.added():
+                output.write('--added--\n')
+                safe_writelines(output, [item + '\n' for item in company_diff.added()])
+                output.write('\n')
+            if company_diff.removed():
+                output.write('--removed--\n')
+                safe_writelines(output, [item + '\n' for item in company_diff.removed()])
+                output.write('\n')
+
+
+def dump_yaml(db, filename):
+    with open(filename, 'w+') as f:
+        yaml.safe_dump(db, f, encoding='utf-8', default_flow_style=False)
+
+
+def record_all_joblistings():
+    with open('company_urls.txt', 'r') as company_urls:
+        data = yaml.load(company_urls)
+        for company_url, job_url in data.items():
+            if 'http' in job_url:
+                find_jobs_on_page(company_url,job_url)
+        Jobs_Database['No Jobs Found'] = []
+        for company_url, job_url in data.items():
+            if job_url not in Jobs_Database or Jobs_Database[job_url] == {}:
+                Jobs_Database['No Jobs Found'].append(job_url)
+    dump_yaml(Jobs_Database, 'joblistings_current.txt')
+
+
+def find_new_jobs():
+    os.rename('joblistings_current.txt', 'joblistings_old.txt')
+    record_all_joblistings()
+    compare_joblists('joblistings_old.txt', 'joblistings_current.txt')
+    os.remove('joblistings_old.txt')
+
+
+job_words = ['Designer', 'Artist', 'Programmer', 'Engineer', 'Director', 'Animator', 'Producer', 'Developer',
+             'Assurance', 'Marketing', 'Intern', 'Administrator', 'Tester', 'Manager', 'QA', 'Development', 'Lead']
+job_exceptions = ['skills', 'Qualifications', 'Requirements', 'Responsibilities', 'career', 'please', 'hiring', 'broad',
+                  'Experience', 'motivate', 'Flexible', 'salary']
+
+
+Jobs_Database = {}
+
+find_new_jobs()
+#print(find_jobs_on_page(r"http://www.buzztime.com", r"http://www.buzztime.com/business/join-team"))
+#print(Jobs_Database)
+
